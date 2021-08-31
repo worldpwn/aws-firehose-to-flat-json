94ef67f8b6f40736e5ffeff6d53485ec139ab7b5
Andrey Kniazev <worldpwn@hotmail.com>
2021-08-31 11:58:22 +0200

    add one level test for multi value object

--- test_flatten_json_object.py
+++ test_flatten_json_object.py
@@ -18,6 +18,13 @@ def test_levelTwo_oneLevelObject_shouldSameValue():
 
 def test_levelOne_oneLevelMultiValueObject_shouldStringifyValue():
     obj = {"value": 12, "other": [1, 3]}
+    level = 1
+    result = flatten_json_object(obj, level)
+    assert result == {"value": "12", "other": "[1, 3]"}
+
+
+def test_levelTwo_oneLevelMultiValueObject_shouldStringifyValue():
+    obj = {"value": 12, "other": [1, 3]}
     level = 2
     result = flatten_json_object(obj, level)
     assert result == {"value": 12, "other": [1, 3]}
